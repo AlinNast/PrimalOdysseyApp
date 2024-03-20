b2d3e52e391d8aebb5890223edcba60a26052224
NastyTester <70013669+AlinNast@users.noreply.github.com>
2024-03-20 23:09:25 +0200

    update dummy data

--- DBImitation/models.py
+++ DBImitation/models.py
@@ -8,6 +8,9 @@ class User(object):
         self.level = level
         self.profile_picture = profile_picture
         self.token = token
+        self.creator = False
+        self.admin = False
+        self.premium = False
         
 
 class LearningTree(object):
@@ -24,10 +27,53 @@ class UserLearningTree(object):
         self.user_id = user_id
         self.learning_tree_id = learning_tree_id
         
+
+
 class Lesson(object):
-    def __init__(self, id, learning_tree_id, title, description, image):
+    def __init__(self, id, learning_tree_id, lesson_requirements_id, title, description, image):
         self.id = id
         self.learning_tree_id = learning_tree_id
+        self.lesson_requirements_id = lesson_requirements_id
         self.title = title
         self.description = description
         self.image = image
+
+
+class LessonRequirement(object):
+    def __init__(self, id, lesson_id1=None, lesson_id2=None, lesson_id3=None, learning_tree_id1=None, learning_tree_id2=None):
+        self.id = id
+        self.lesson_id1 = lesson_id1
+        self.lesson_id2 = lesson_id2
+        self.lesson_id3 = lesson_id3
+        self.learning_tree_id1 = learning_tree_id1
+        self.learning_tree_id2 = learning_tree_id2
+
+
+class Achievement(object):
+    def __init__(self, id, name, description, icon, achievement_requirement_id):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.icon = icon
+        self.requirement_id = achievement_requirement_id
+
+class UserAchievement(object):
+    def __init__(self, id, user_id, achievement_id):
+        self.id = id
+        self.user_id = user_id
+        self.achievement_id = achievement_id
+
+
+
+class AchievementRequirement(object):
+    """
+    Represents the requirement for an achievement, 
+    it will have a unique id and a list of ids of required achievements
+    """
+    def __init__(self, id, lesson_id1=None, lesson_id2=None, lesson_id3=None, learning_tree_id1=None, learning_tree_id2=None):
+        self.id = id
+        self.lesson_id1 = lesson_id1
+        self.lesson_id2 = lesson_id2
+        self.lesson_id3 = lesson_id3
+        self.learning_tree_id1 = learning_tree_id1
+        self.learning_tree_id2 = learning_tree_id2
--- repository.py
+++ repository.py
@@ -1,4 +1,4 @@
-from DBImitation.models import User, LearningTree, UserLearningTree, Lesson
+from DBImitation.models import User, LearningTree, UserLearningTree, Lesson, LessonRequirement, Achievement, AchievementRequirement, UserAchievement
 
 
 def get_user_by_id(id):
@@ -37,6 +37,16 @@ def get_lesson_by_id(lesson_id):
     for lesson in DummyRepository().lessons:
         if lesson.id == lesson_id:
             return lesson
+        
+        
+
+def get_user_achievements(user_id):
+    user_achievements = []
+    for user_achievement in DummyRepository().user_achivements:
+        if user_achievement.user_id == user_id:
+            user_achievements.append(DummyRepository().achievements[user_achievement.achievement_id-1])
+    return user_achievements
+
 
 
 class DummyRepository:
@@ -57,9 +67,36 @@ class DummyRepository:
         ]
         
         self.lessons = [
-            Lesson(id=1, learning_tree_id=1, title="Lesson 1", description="Introduction to Python basics", image="lesson1_image.jpg"),
-            Lesson(id=2, learning_tree_id=1, title="Lesson 2", description="Data types and variables", image="lesson2_image.jpg"),
-            Lesson(id=3, learning_tree_id=1, title="Lesson 3", description="Control flow and loops", image="lesson3_image.jpg"),
-            Lesson(id=4, learning_tree_id=1, title="Lesson 4", description="Functions and modules", image="lesson4_image.jpg"),
-            Lesson(id=5, learning_tree_id=1, title="Lesson 5", description="Advanced topics", image="lesson5_image.jpg"),
+            Lesson(id=1, learning_tree_id=1,lesson_requirements_id=1, title="Lesson 1", description="Introduction to Python basics", image="lesson1_image.jpg"),
+            Lesson(id=2, learning_tree_id=1,lesson_requirements_id=2, title="Lesson 2", description="Data types and variables", image="lesson2_image.jpg"),
+            Lesson(id=3, learning_tree_id=1,lesson_requirements_id=3, title="Lesson 3", description="Control flow and loops", image="lesson3_image.jpg"),
+            Lesson(id=4, learning_tree_id=1,lesson_requirements_id=4, title="Lesson 4", description="Functions and modules", image="lesson4_image.jpg"),
+            Lesson(id=5, learning_tree_id=1,lesson_requirements_id=5, title="Lesson 5", description="Advanced topics", image="lesson5_image.jpg"),
+        ]
+        
+        
+        self.lesson_requirements = [
+            LessonRequirement(lesson_id=1, required_lesson_id=None),  # no requirement
+            LessonRequirement(lesson_id=2, required_lesson_id=None),  # requires Lesson 1
+            LessonRequirement(lesson_id=3, required_lesson_id=None),  # requires Lesson 2
+            LessonRequirement(lesson_id=4, required_lesson_id=3),  # requires Lesson 3
+            LessonRequirement(lesson_id=5, required_lesson_id=4),  # no requirement
+        ]
+
+        
+        self.achivements = [
+            Achievement(id=1, title="Achievement 1", description="This is the first achievement description placeholder", image="achiv1_image.jpg", achievement_requirement_id=1),
+            Achievement(id=2, title="Achievement 2", description="This is the second achievement description placeholder", image="achiv2_image.jpg", achievement_requirement_id=2),
+            Achievement(id=3, title="Achievement 3", description="This is the third achievement description placeholder", image="achiv3_image.jpg", achievement_requirement_id=3),
+        ]
+
+        
+        self.achivement_requirements = [
+            AchievementRequirement(1),
+            AchievementRequirement(2, 1),
+            AchievementRequirement(3, 5)
+        ]
+        
+        self.user_achivements = [
+            UserAchievement(id=1, user_id=1, achievement_id=1),
         ]
